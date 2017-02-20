70de96dfad6ba7ead5ce33220d402d8e45b81dbd
CELIA <c.martinher@outlook.es>
2017-02-20 20:19:23 +0100

    Ejercicio 14.6 Urls aleat resuelto

---
+++ aleat.py
@@ -0,0 +1,15 @@
+
+import webapp
+import random
+
+
+class urlsaleat(webapp.webApp):
+	"""docstring for urlsaleat"""
+	def process (self, parsedRequest):
+		num_aleat = random.randrange(1000)
+		return ("200 OK", "<html><body><h1> Hola <a href=" + str(num_aleat) + ">dame otra URL</a></h1></body></html>")
+
+if __name__ == "__main__":
+    testWebApp = urlsaleat("localhost", 1234)
+		
+
